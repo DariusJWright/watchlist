c748b2613297af5133398e40bcff5d95e1491f09
darshnablah <dwright758@gmail.com>
2021-01-21 12:32:09 -0600

    created typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,43 @@ const { gql } = require('apollo-server-express');
 
 // create typeDefs
 const typeDefs = gql`
+  type Movie {
+    _id: ID
+    movieId: Int
+    posterURL: String
+    title: String
+  }
+
+  type User {
+    _id: ID!
+    username: String
+    password: String
+    movieCount: Int
+    savedMovies: [Movie]
+  }
+
   type Query {
-    helloWorld: String
+    me: User
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(username: String!, password: String!): User
+    addUser(username: String!, password: String!): User
+    addMovie(movie: addMovieInput): User
+    removeMovie(movieId: Int!): User
+  }
+
+  input addMovieInput {
+    movieId: Int
+    title: String
+    posterURL: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 // import express, mongoose and apollo
 const express = require('express');
 const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// create a new Apollo server and pass in schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate Apollo server with Express as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -11,5 +24,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log GQL Playground URL
+    console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
