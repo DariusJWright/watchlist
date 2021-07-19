0972e2f1f66dcdcf0462872ed1b39496a62c6471
darshnablah <dwright758@gmail.com>
2021-07-19 14:47:59 -0500

    refactored a bit

--- client/src/components/Movies/index.js
+++ client/src/components/Movies/index.js
@@ -2,18 +2,11 @@ import { useState, useEffect } from 'react';
 import { getPopularMovieData } from '../../utils/api';
 import Card from '../Card';
 
-const Popular = () => {
-  const [popularMovieData, setPopularMovieData] = useState([]);
-  console.log(popularMovieData);
-  useEffect(async () => {
-    const data = await getPopularMovieData();
-    setPopularMovieData(data);
-  }, []);
-
+const Movies = ({ popular }) => {
   return (
     <div className='container'>
       <h2>Top 20 in USA</h2>
-      {popularMovieData.map(movie => (
+      {popular.map(movie => (
         <Card
             overview={movie.overview}
             posterPath={movie.poster_path}
@@ -24,4 +17,4 @@ const Popular = () => {
   );
 }
 
-export default Popular;
+export default Movies;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,20 @@
-import Popular from '../components/Popular';
+import { useState, useEffect } from 'react';
+import { getPopularMovieData } from '../utils/api';
+import Movies from '../components/Movies';
 import Search from '../components/Search';
 
 const Home = () => {
+  const [popularMovieData, setPopularMovieData] = useState([]);
+  console.log(popularMovieData);
+  useEffect(async () => {
+    const data = await getPopularMovieData();
+    setPopularMovieData(data);
+  }, []);
+
   return (
     <div>
       <Search />
-      <Popular />
+      <Movies popular={popularMovieData}/>
     </div>
   )
 }
